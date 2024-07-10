6691bb52f34af8465a8a03a9d28ae4a60c24e6f8
Brian T. <bptiburcio@gmail.com>
2024-07-10 19:01:39 -0400

    Adds handlebar views; layouts and homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+
+// GET all galleries for homepage
+router.get('/', async (req, res) => {
+  res.render('index');
+  // try {
+  //   const dbGalleryData = await Gallery.findAll({
+  //     include: [
+  //       {
+  //         model: Painting,
+  //         attributes: ['filename', 'description'],
+  //       },
+  //     ],
+  //   });
+
+  //   const galleries = dbGalleryData.map((gallery) =>
+  //     gallery.get({ plain: true })
+  //   );
+  //   res.render('homepage', {
+  //     galleries,
+  //     loggedIn: req.session.loggedIn,
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  //   res.status(500).json(err);
+  // }
+});

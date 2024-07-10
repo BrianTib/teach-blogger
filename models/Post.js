e93460bbbf65d739afed7a72259002afdb86deae
Brian T. <bptiburcio@gmail.com>
2024-07-10 04:36:38 -0400

    Initial commit

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+        type: DataTypes.INTEGER,
+        unique: false,
+        references: {
+            model: 'user',
+            key: 'id',
+        }
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;

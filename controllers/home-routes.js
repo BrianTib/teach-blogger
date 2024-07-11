2673cbe9a8fe1cf610875a84a97da8727182c5d3
Brian T. <bptiburcio@gmail.com>
2024-07-10 22:38:58 -0400

    Removes jass. Adds styling for login and signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,27 +4,14 @@ const { User, Post } = require('../models');
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
   res.render('homepage');
-  // try {
-  //   const dbGalleryData = await Gallery.findAll({
-  //     include: [
-  //       {
-  //         model: Painting,
-  //         attributes: ['filename', 'description'],
-  //       },
-  //     ],
-  //   });
+});
 
-  //   const galleries = dbGalleryData.map((gallery) =>
-  //     gallery.get({ plain: true })
-  //   );
-  //   res.render('homepage', {
-  //     galleries,
-  //     loggedIn: req.session.loggedIn,
-  //   });
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(500).json(err);
-  // }
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 module.exports = router;

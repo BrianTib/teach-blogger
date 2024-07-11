978b8f2a340fbc252ca41b65d81d93105517be73
Brian T. <bptiburcio@gmail.com>
2024-07-10 21:34:55 -0400

    Adds styling for navbar

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const { User, Post } = require('../models');
 
 // GET all galleries for homepage
 router.get('/', async (req, res) => {
-  res.render('index');
+  res.render('homepage');
   // try {
   //   const dbGalleryData = await Gallery.findAll({
   //     include: [
